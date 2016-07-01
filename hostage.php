7d8d37fb4bdc0e5b0b77f44fb06286bb38c3d0f6
Diyanada J. Gunawardena <diyanada@gmail.com>
2016-07-02 03:50:37 +0530

    int

---
+++ hostage.php
@@ -0,0 +1,12 @@
+<?php 
+
+//******************************************************************************
+//	Diyanada J. Gunawardena
+// 	diyanada@gmail.com
+// 	Dynamete Software Solutions
+//******************************************************************************
+
+
+include_once (dirname(__FILE__) . "/default.xhtml");
+
+
